10ea9a4ad36c9dd7a52118d3330a3086c7db6da0
Zibari <zibari@Dilans-MacBook-Air.local>
2021-04-14 13:31:40 +0300

    Arrays of red,green and blue

---
+++ js/script-image.js
@@ -0,0 +1,38 @@
+readImg(document.getElementById("staticImg"));
+
+function readImg(image) {
+    var img = image;
+    let redD = {}, greenD ={}, blueD ={};
+
+    img.onload = function(){
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+    
+        canvas.height = img.height;
+        canvas.width = img.width;
+
+        context.drawImage(img, 0, 0);
+
+        var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+        var pixelData = imgData.data;
+        //console.log(pixelData);
+
+        for (var i = 0; i < 256; i++) { //an img might not have all 256 values of each color
+            redD[i] = 0;
+            greenD[i] = 0;
+            blueD[i] = 0;
+          }
+        for (var i = 0; i < pixelData.length; i += 4) { //moving forward pixel by pixel
+            redD[pixelData[i]]++;
+            greenD[pixelData[i + 1]]++;
+            blueD[pixelData[i + 2]]++;
+          }
+        
+          console.log(redD);
+    }
+
+
+}
+ 
+
+
